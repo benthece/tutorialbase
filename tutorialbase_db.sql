5842dd1d95c26e3c062c0dd95088aaa7604c7317
GeriV16 <gepike0416@gmail.com>
2025-01-17 13:27:21 +0100

    Adatbázis (#5)
    * Adatbázis
    
    Adatbázis táblák, teszt adatok feltöltve + tárolt eljárások
    
    * DataGrid beállítások
    
    * Backend bence (#1)
    
    * Adatbázis
    
    Adatbázis táblák, teszt adatok feltöltve + tárolt eljárások
    
    * DataGrid beállítások
    
    * Backend bence (#2)
    
    * Adatbázis
    
    Adatbázis táblák, teszt adatok feltöltve + tárolt eljárások
    
    * DataGrid beállítások
    
    * Reset wish tárolt eljárás
    
    * Adatbázis javítások
    
    Random főkategória generálás, példa adat és táblaszerkezet javítás
    
    * véletlen alkategória generálás
    
    * Sample data mod, + tárolt eljárások
    
    * get profile tárolt eljárás
    
    + comment add timestamps
    
    * Adatbazis update
    
    ---------
    
    Co-authored-by: benthece <mememail@keemail.me>
    Co-authored-by: Jegenyés Bence <155091389+benthece@users.noreply.github.com>

---
+++ tutorialbase_db.sql
@@ -0,0 +1,161 @@
+DROP DATABASE IF EXISTS `tutorialbase`;
+
+CREATE DATABASE `tutorialbase`;
+
+USE `tutorialbase`;
+
+CREATE TABLE `users`
+(
+    `id`              INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `username`        VARCHAR(20)                         NOT NULL,
+    `email`           VARCHAR(32)                         NOT NULL,
+    `password`        CHAR(32)                         NOT NULL,              -- SHA3-128
+    `profile_pic_url` VARCHAR(50),
+    `bg_image_url`    VARCHAR(50),
+    `bio`             VARCHAR(100)                        NOT NULL,
+    `privilege_id`    TINYINT UNSIGNED                    NOT NULL,
+    `wishes`          TINYINT UNSIGNED                    NOT NULL DEFAULT 5,
+    `last_login`      TIMESTAMP                           NOT NULL,
+    `pw_modified_at`  TIMESTAMP                           NULL     DEFAULT NULL, -- alapértelmezetten NULL
+    `created_at`      TIMESTAMP                                    DEFAULT NOW(),
+    `is_deleted`      BOOLEAN                             NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `user_privileges`
+(
+    `id`   TINYINT UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(13)
+);
+
+CREATE TABLE `videos`
+(
+    `id`             INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `title`          VARCHAR(32),
+    `description`    VARCHAR(100)                        NOT NULL,
+    `url`            VARCHAR(50),
+    `base_image_url` VARCHAR(50),
+    `is_deleted`     BOOLEAN                             NOT NULL DEFAULT 0,
+    `views`          INTEGER UNSIGNED                    NOT NULL DEFAULT 0,
+    `user_id`        INTEGER UNSIGNED,
+    `uploaded_at`    TIMESTAMP                                    DEFAULT NOW(),
+    `modified_at`    TIMESTAMP                           NULL     DEFAULT NULL -- ez is direkt null
+);
+
+CREATE TABLE `comments`
+(
+    `id`          INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `user_id`     INTEGER UNSIGNED,
+    `video_id`    INTEGER UNSIGNED,
+    `text`        VARCHAR(100),
+    `created_at`  TIMESTAMP                                    DEFAULT NOW(),
+    `modified_at` TIMESTAMP                           NULL     DEFAULT NULL, -- alapértelmezetten NULL
+    `is_deleted`  BOOLEAN                             NOT NULL DEFAULT 0
+);
+
+CREATE TABLE `reactions`
+(
+    `user_id`    INTEGER UNSIGNED,
+    `video_id`   INTEGER UNSIGNED,
+    `is_useful`  BOOLEAN NOT NULL, -- direkt nincs alapértelmezett érték
+    `is_removed` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user_id`, `video_id`)
+);
+
+CREATE TABLE `categories`
+(
+    `id`        INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `parent_id` INTEGER UNSIGNED, -- valszeg csak úgy jó, ha null értéket kap a főkategória
+    `name`      VARCHAR(30)
+);
+
+CREATE TABLE `wish`
+(
+    `id`      INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `text`    VARCHAR(40),
+    `user_id` INTEGER UNSIGNED
+);
+
+CREATE TABLE `tags`
+(
+    `id`   INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `text` VARCHAR(20)
+);
+
+CREATE TABLE `watch_history`
+(
+    `video_id`   INTEGER UNSIGNED,
+    `user_id`    INTEGER UNSIGNED,
+    `viewed_at`  TIMESTAMP        DEFAULT NOW(),
+    `is_deleted` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`video_id`, `user_id`)
+);
+
+ALTER TABLE `categories`
+    ADD FOREIGN KEY (`parent_id`) REFERENCES `categories` (`id`);
+
+ALTER TABLE `users`
+    ADD FOREIGN KEY (`privilege_id`) REFERENCES `user_privileges` (`id`);
+
+ALTER TABLE `videos`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `comments`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `comments`
+    ADD FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`);
+
+ALTER TABLE `reactions`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `reactions`
+    ADD FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`);
+
+CREATE TABLE `video_category`
+(
+    `category_id` INTEGER UNSIGNED,
+    `video_id`    INTEGER UNSIGNED,
+    PRIMARY KEY (`category_id`, `video_id`)
+);
+
+ALTER TABLE `video_category`
+    ADD FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+ALTER TABLE `video_category`
+    ADD FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`);
+
+ALTER TABLE `watch_history`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `watch_history`
+    ADD FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`);
+
+ALTER TABLE `wish`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+CREATE TABLE `videos_tags`
+(
+    `video_id` INTEGER UNSIGNED,
+    `tag_id`   INTEGER UNSIGNED,
+    PRIMARY KEY (`video_id`, `tag_id`)
+);
+
+ALTER TABLE `videos_tags`
+    ADD FOREIGN KEY (`video_id`) REFERENCES `videos` (`id`);
+
+ALTER TABLE `videos_tags`
+    ADD FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`);
+
+CREATE TABLE `users_wish_upvote`
+(
+    `user_id`    INTEGER UNSIGNED,
+    `wish_id`    INTEGER UNSIGNED,
+    `is_deleted` BOOLEAN NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user_id`, `wish_id`)
+);
+
+ALTER TABLE `users_wish_upvote`
+    ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `users_wish_upvote`
+    ADD FOREIGN KEY (`wish_id`) REFERENCES `wish` (`id`);
