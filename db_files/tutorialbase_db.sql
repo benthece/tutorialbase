4ae8a5a2ef0a8ca3a8bd5077683659ad1b923171
benthece <mememail@keemail.me>
2025-01-20 15:18:51 +0100

    Felesleges uuid függvény törlés

--- db_files/tutorialbase_db.sql
+++ db_files/tutorialbase_db.sql
@@ -30,32 +30,6 @@ END;
 $$
 DELIMITER ;
 
-DELIMITER $$
-CREATE FUNCTION uuid_v4s()
-    RETURNS CHAR(32)
-BEGIN
-    -- 1th and 2nd block are made of 6 random bytes
-    SET @h1 = HEX(RANDOM_BYTES(4));
-    SET @h2 = HEX(RANDOM_BYTES(2));
-
-    -- 3th block will start with a 4 indicating the version, remaining is random
-    SET @h3 = SUBSTR(HEX(RANDOM_BYTES(2)), 2, 3);
-
-    -- 4th block first nibble can only be 8, 9 A or B, remaining is random
-    SET @h4 = CONCAT(HEX(FLOOR(ASCII(RANDOM_BYTES(1)) / 64) + 8),
-                     SUBSTR(HEX(RANDOM_BYTES(2)), 2, 3));
-
-    -- 5th block is made of 6 random bytes
-    SET @h5 = HEX(RANDOM_BYTES(6));
-
-    -- Build the complete UUID
-    RETURN LOWER(CONCAT(
-            @h1, @h2, '4', @h3, @h4, @h5
-                 ));
-END;
-$$
-DELIMITER ;
-
 CREATE TABLE `users`
 (
     `id`              INTEGER UNSIGNED UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
