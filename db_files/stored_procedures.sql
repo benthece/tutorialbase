9bcbbf902cbe42534f9c37a2b7208f2d6dde63e8
benthece <mememail@keemail.me>
2025-03-04 13:44:08 +0100

    Add video guid generation

--- db_files/stored_procedures.sql
+++ db_files/stored_procedures.sql
@@ -4,7 +4,33 @@ DELIMITER $$
 CREATE OR REPLACE TRIGGER uuid_v4s BEFORE INSERT ON users FOR EACH ROW -- UUID V4 generáláshoz
     -- RETURNS CHAR(36)
 BEGIN
-    -- 1th and 2nd block are made of 6 random bytes
+    -- 1st and 2nd block are made of 6 random bytes
+    SET @h1 = HEX(RANDOM_BYTES(4));
+    SET @h2 = HEX(RANDOM_BYTES(2));
+
+    -- 3th block will start with a 4 indicating the version, remaining is random
+    SET @h3 = SUBSTR(HEX(RANDOM_BYTES(2)), 2, 3);
+
+    -- 4th block first nibble can only be 8, 9 A or B, remaining is random
+    SET @h4 = CONCAT(HEX(FLOOR(ASCII(RANDOM_BYTES(1)) / 64) + 8),
+                     SUBSTR(HEX(RANDOM_BYTES(2)), 2, 3));
+
+    -- 5th block is made of 6 random bytes
+    SET @h5 = HEX(RANDOM_BYTES(6));
+
+    -- Build the complete UUID
+    SET NEW.guid = LOWER(CONCAT(
+            @h1, '-', @h2, '-', '4', @h3, '-', @h4, '-', @h5
+                 ));
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE OR REPLACE TRIGGER uuid_v4s_vid BEFORE INSERT ON videos FOR EACH ROW -- UUID V4 generáláshoz
+    -- RETURNS CHAR(36)
+BEGIN
+    -- 1st and 2nd block are made of 6 random bytes
     SET @h1 = HEX(RANDOM_BYTES(4));
     SET @h2 = HEX(RANDOM_BYTES(2));
 
